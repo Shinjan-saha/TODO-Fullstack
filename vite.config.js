bd3436bd2f62757c0b1057296e72338aff4573ce
Shinjan-saha <shinjansaha02@gmail.com>
2024-07-21 19:28:57 +0530

    Added PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,65 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      selfDestroying: true,
+      manifest: {
+        short_name: "TODO",
+        name: "TodoList",
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/todologo.png",
+            sizes: "160x160",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+
+      workbox: {
+        cleanupOutdatedCaches: true,
+        skipWaiting: true,
+        clientsClaim: true,
+
+        runtimeCaching: [
+          {
+            urlPattern: new RegExp(
+              "^https://fonts.(?:googleapis|gstatic).com/(.*)"
+            ),
+            handler: "CacheFirst",
+            options: {
+              cacheName: "google-fonts",
+              expiration: {
+                maxEntries: 30,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+
+          {
+            urlPattern: /\.(?:png|gif|jpg|jpeg|svg|webp)$/,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "images",
+              expiration: {
+                maxEntries: 60,
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
